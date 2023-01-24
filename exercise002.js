b49eef241e6c53b0cd3700c5a855e899283ec087
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2023-01-24 18:58:27 +0000

    Exercise 2, extension. Cat facts!

--- exercise002.js
+++ exercise002.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const catFacts = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1";
 
 /*const fetchData = (apiEndPoint) => {
   fetch(apiEndPoint)
@@ -21,4 +22,17 @@ const fetchData = async (apiEndPoint) => {
   }
 }
 
-fetchData(jsonTypicode);
+//fetchData(jsonTypicode);
+
+// This seemed good in theory. The amount of actual facts vs random gibberish is depressingly small!
+const fetchCatFacts = async (apiEndPoint) => {
+  try {
+    const response = await fetch(apiEndPoint);
+    const jsonData = await response.json();
+    console.log(jsonData.text);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchCatFacts(catFacts);
